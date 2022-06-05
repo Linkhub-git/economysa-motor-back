bdae9d48cff14c56382ddd536d711d57e9de1771
Jeferson Cieza <jefferson.cieza@pucp.edu.pe>
2022-06-05 17:49:14 -0500

    Se agrega documentación del endpoint Giro de Negocio

--- src/main/java/com/economysa/motor/app/configuration/controller/EntryController.java
+++ src/main/java/com/economysa/motor/app/configuration/controller/EntryController.java
@@ -2,6 +2,11 @@ package com.economysa.motor.app.configuration.controller;
 
 import com.economysa.motor.app.configuration.entity.Entry;
 import com.economysa.motor.app.configuration.service.EntryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(
+    name = "Giros de negocio",
+    description = "Endpoint donde se encuentran las operaciones" +
+        " respecto al recurso Giro de negocio."
+)
 @RestController
 @RequestMapping("/api/v1/secured/entry")
 public class EntryController {
@@ -19,13 +29,51 @@ public class EntryController {
   @Autowired
   private EntryService service;
 
+  @Operation(
+      description = "Listado de Giros de Negocio",
+      summary = "Listado de Giros de Negocio",
+      method = "GET"
+  )
+  @ApiResponses(
+      {
+          @ApiResponse(
+              responseCode = "200",
+              description = "Operación exitosa"
+          ),
+          @ApiResponse(
+              responseCode = "500",
+              description = "Error interno del sistema"
+          )
+      }
+  )
   @GetMapping
   public ResponseEntity<List<Entry>> list(@RequestParam Long listPrice) {
     return new ResponseEntity<>(service.listByListPrice(listPrice), HttpStatus.OK);
   }
 
+  @Operation(
+      description = "Búsqueda de Giros de negocio ingresando un" +
+          " parámetro opcional",
+      summary = "Búsqueda de Giros de negocio ingresando un" +
+          " parámetro opcional",
+      method = "GET"
+  )
+  @ApiResponses(
+      {
+          @ApiResponse(
+              responseCode = "200",
+              description = "Operación exitosa"
+          ),
+          @ApiResponse(
+              responseCode = "500",
+              description = "Error interno del sistema"
+          )
+      }
+  )
   @GetMapping("/search")
-  public ResponseEntity<List<Entry>> search(@RequestParam String query) {
+  public ResponseEntity<List<Entry>> search(
+      @Parameter(name = "query", description = "Texto de búsqueda")
+      @RequestParam String query) {
     return new ResponseEntity<>(service.search(query), HttpStatus.OK);
   }
 }
